5df6182d281c4ec75c0abe32be5dac50086e75a4
Shanjai raj S <shanjairaj08@gmail.com>
2026-07-07 10:44:47 +0530

    Add Booking controller for Book My Show app

---
+++ book-my-show-app/controller/BookingController.java
@@ -0,0 +1,13 @@
+public class BookingController {
+
+    public void bookTicket(Booking booking) {
+        System.out.println("Booking successful!");
+        System.out.println("Booking ID: " + booking.getBookingId());
+        System.out.println("Number of seats: " + booking.getNumberOfSeats());
+        System.out.println("Total amount: " + booking.getTotalAmount());
+    }
+
+    public void cancelBooking(Booking booking) {
+        System.out.println("Booking " + booking.getBookingId() + " cancelled successfully!");
+    }
+}
