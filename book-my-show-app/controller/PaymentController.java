a33e4b8cd251ac74dc65284dbe9d049fb8c7c7d1
Shanjai raj S <shanjairaj08@gmail.com>
2026-07-07 10:51:10 +0530

    Add Payment controller for Book My Show app

---
+++ book-my-show-app/controller/PaymentController.java
@@ -0,0 +1,15 @@
+public class PaymentController {
+
+    public void processPayment(Payment payment) {
+        System.out.println("Processing payment...");
+        System.out.println("Payment ID: " + payment.getPaymentId());
+        System.out.println("Amount: " + payment.getAmount());
+        System.out.println("Payment Method: " + payment.getPaymentMethod());
+        System.out.println("Payment Status: " + payment.getPaymentStatus());
+    }
+
+    public void refundPayment(Payment payment) {
+        System.out.println("Refunding payment ID: " + payment.getPaymentId());
+        System.out.println("Refund amount: " + payment.getAmount());
+    }
+}
